96468af520ef24e3ca4633c959cff8075b394a09
adelbbj <adelbbj@gmail.com>
2025-06-04 16:39:29 +0330

    feat(types): add types of shared contacts and api requests

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+// src/types/index.ts
+export interface Contact {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string | null;
+  gender: string;
+  phone: string;
+  note: string;
+  telegram: string | null;
+  avatar: string;
+  company: string;
+  address: string | null;
+  createdAt: number;
+  updatedAt: number;
+}
+
+export interface ApiResponse {
+  meta: {
+    skipped: number;
+    limit: number;
+    total: number;
+    criteria: Record<string, unknown>;
+  };
+  items: Contact[];
+}
+
+export interface SearchFilters {
+  first_name?: string;
+  last_name?: string;
+  phone?: string;
+}
+
+export interface Pagination {
+  skip: number;
+  limit: number;
+}
